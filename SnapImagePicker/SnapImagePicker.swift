03b1fd81c4a87df92cd920831fbdc71477535817
Esten Leonardsen <estenleonardsen@gmail.com>
2016-07-20 22:21:09 +0200

    Changed default font size

--- SnapImagePicker/SnapImagePicker.swift
+++ SnapImagePicker/SnapImagePicker.swift
@@ -18,7 +18,7 @@ public class SnapImagePicker {
     public struct Theme {
         public static var color = UIColor.init(red: 0xFF, green: 0x00, blue: 0x58, alpha: 1)
         public static var maxImageSize = 2000
-        public static var fontSize = CGFloat(22.0)
+        public static var fontSize = CGFloat(20.0)
         public static var font = SnapFonts.gothamRoundedMediumOfSize(fontSize)
     }
     
