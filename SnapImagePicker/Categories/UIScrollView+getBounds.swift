0fff3d0ac093a56d384a52166494c722d494633d
Esten Leonardsen <estenleonardsen@gmail.com>
2016-07-28 22:18:04 +0200

    Fixed erronous croprect of zoomed image

--- SnapImagePicker/Categories/UIScrollView+getBounds.swift
+++ SnapImagePicker/Categories/UIScrollView+getBounds.swift
@@ -3,28 +3,29 @@ import UIKit
 extension UIScrollView  {
     func getImageBoundsForImageView(imageView: UIImageView?) -> CGRect? {
         if let imageView = imageView,
-            let image = imageView.image {
-                let visibleRect = self.convertRect(self.bounds, toView: imageView)
-                let ratio = max(image.size.width, image.size.height) / imageView.bounds.width
-                let transformedVisibleRect = CGRect(x: visibleRect.minX * ratio,
-                                                    y: visibleRect.minY * ratio,
-                                                    width: visibleRect.width * ratio,
-                                                    height: visibleRect.height * ratio)
+           let image = imageView.image {
+            let visibleRect = self.convertRect(self.bounds, toView: imageView)
+            let verticalRatio = image.size.height / imageView.bounds.height
+            let horizontalRatio = image.size.width / imageView.bounds.width
+            let transformedVisibleRect = CGRect(x: visibleRect.minX * horizontalRatio,
+                                                y: visibleRect.minY * horizontalRatio,
+                                                width: visibleRect.width * verticalRatio,
+                                                height: visibleRect.height * verticalRatio)
     
-                var verticalOffset = CGFloat(0.0)
-                var horizontalOffset = CGFloat(0.0)
-                if image.size.width > image.size.height {
-                    verticalOffset = (image.size.width - image.size.height) / 2
-                } else {
-                    horizontalOffset = (image.size.height - image.size.width) / 2
-                }
+            var verticalOffset = CGFloat(0.0)
+            var horizontalOffset = CGFloat(0.0)
+            if image.size.width > image.size.height {
+                verticalOffset = (image.size.width - image.size.height) / 2
+            } else {
+                horizontalOffset = (image.size.height - image.size.width) / 2
+            }
     
-                let cropRect = CGRect(x: transformedVisibleRect.minX - horizontalOffset,
-                                      y: transformedVisibleRect.minY - verticalOffset,
-                                      width: transformedVisibleRect.width,
-                                      height: transformedVisibleRect.height)
+            let cropRect = CGRect(x: transformedVisibleRect.minX - horizontalOffset,
+                                  y: transformedVisibleRect.minY - verticalOffset,
+                                  width: transformedVisibleRect.width,
+                                  height: transformedVisibleRect.height)
     
-                return cropRect
+            return cropRect
             }
     
         return nil
